878e7bfa9140a218da9ba4055080e400c994ceed
colemankus <hanshi2929@gmail.com>
2024-01-23 13:10:44 -0600

    added unit information page

--- ListBuilder-MERN/client/src/App.jsx
+++ ListBuilder-MERN/client/src/App.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddUnit from "./pages/AddUnit";
-import ShowList from "./pages/ShowList";
 import DeleteUnit from "./pages/DeleteUnit";
 import EditUnit from "./pages/EditUnit";
+import UnitDetails from "./pages/UnitDetails";
 
 
 const App = () => {
@@ -12,9 +12,9 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/units/create' element={<AddUnit />}/>
-      <Route path='/units/list' element={<ShowList />}/>
       <Route path='/units/delete/:id' element={<DeleteUnit />}/>
       <Route path='/units/edit/:id' element={<EditUnit />}/>
+      <Route path='/units/details/:id' element={<UnitDetails />}/>
     </Routes>
   )
 }
--- ListBuilder-MERN/client/src/pages/ShowList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ShowList = () => {
-  return (
-    <div>ShowList</div>
-  )
-}
-
-export default ShowList
